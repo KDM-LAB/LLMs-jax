2c4fd4f7d72018f327b7cdfeacd4fff15fe427ff
Divya Patel <divyapatel0273@gmail.com>
2024-01-22 19:47:44 +0530

    update

--- scripts/downloadall.py
+++ scripts/downloadall.py
@@ -37,10 +37,6 @@ def downloadall():
 
         print(f'Downloading {pickle_name} from {repo_name}')
         api = HfApi()
-        
-        # download the file
-        api.download_repo(repo_name, './', use_auth_token=True)
-
         # download it in /mnt/mydisk/models
         local_filepath = hf_hub_download(repo_id=repo_name, filename=pickle_name, revision='main', output_dir='/mnt/mydisk/models')
 
